6091705489a886c2dea4233ce9a6dce236b140c3
luoweihua7 <luoweihua7@users.noreply.github.com>
2018-05-25 14:32:22 +0800

    Create Zhihu-Url-Fix.user.js

---
+++ user/Zhihu-Url-Fix.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         知乎帖子外链直接打开
+// @version      0.1
+// @description  知乎帖子外链直接打开
+// @author       larify
+// @run-at       document-idle
+// @include      *.zhihu.com/*
+// @downloadURL  https://github.com/luoweihua7/TamperMonkey/raw/master/user/Zhihu-Url-Fix.user.js
+// @grant        none
+
+// ==/UserScript==
