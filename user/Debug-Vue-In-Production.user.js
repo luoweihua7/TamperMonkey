ee6e19d2cdd61cf57227601ffc5175be712ab4b2
luoweihua7 <luoweihua7@users.noreply.github.com>
2019-04-27 20:33:38 +0800

    Create Debug-Vue-In-Production.user.js

---
+++ user/Debug-Vue-In-Production.user.js
@@ -0,0 +1,82 @@
+// ==UserScript==
+// @name         Vue调试
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  在生产环境开启Vue.js devtools调试
+// @author       larify
+// @run-at       document-end
+// @match        <all_urls>
+// @include      *
+// @grant        unsafeWindow
+// @downloadURL  https://github.com/luoweihua7/TamperMonkey/raw/master/user/Debug-Vue-In-Production.user.js
+// @icon         https://vuejs.org//images/logo.png
+// ==/UserScript==
+
+(function() {
+  function findVue($el) {
+    // console.log(`Finding Vue: ${$el.tagName}`)
+    let __vue__ = $el.__vue__;
+
+    if (__vue__) {
+      return __vue__;
+    } else {
+      let tagName = $el.tagName;
+      if (["SCRIPT", "STYLE"].indexOf(tagName) === -1) {
+        let children = [...$el.children];
+
+        children.some($child => {
+          __vue__ = findVue($child);
+          return __vue__;
+        });
+
+        return __vue__;
+      } else {
+        return;
+      }
+    }
+  }
+
+  function getVue(obj) {
+    if (!!obj && obj._isVue) {
+      let $constructor = obj.constructor;
+
+      if (
+        $constructor.config &&
+        typeof $constructor.config.devtools === "boolean"
+      ) {
+        return obj.constructor;
+      }
+
+      if (
+        $constructor.super &&
+        $constructor.super.config &&
+        typeof $constructor.super.config.devtools === "boolean"
+      ) {
+        return $constructor.super;
+      }
+    }
+
+    return;
+  }
+
+  let window = unsafeWindow;
+
+  setTimeout(() => {
+    if (
+      typeof window.__VUE_DEVTOOLS_GLOBAL_HOOK__ === "object" &&
+      typeof __VUE_DEVTOOLS_GLOBAL_HOOK__.emit === "function"
+    ) {
+      let $vm = findVue(document.querySelector("body"));
+      let _Vue = getVue($vm);
+
+      if (_Vue) {
+        _Vue.config.devtools = true;
+        __VUE_DEVTOOLS_GLOBAL_HOOK__.emit("init", _Vue);
+
+        console.log(
+          `已启用Vue生产环境调试，如果无法看到Vue调试Tab，请关闭DevTools再打开`
+        );
+      }
+    }
+  }, 500);
+})();
